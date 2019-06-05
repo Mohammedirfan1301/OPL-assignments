4c81239113b80f7a32e4d92135f1cc80e6430441
Irfan Mohammed <mohammedirfan1460231@gmail.com>
2019-06-05 14:47:54 -0400

    Added Task files

---
+++ Task12.py
@@ -0,0 +1,9 @@
+class typea :
+    def __init__(self,l1):
+        self.l1 = l1
+class typeb:
+    def __init__(self,l2):
+      self.l2 = l2
+class typec:
+    def __init__(self,l3):
+      self.l3 = l3
