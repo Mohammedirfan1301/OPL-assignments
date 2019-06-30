1bf528f12ba9058d8b4871407ed02581746e838b
Irfan Mohammed <mohammedirfan1460231@gmail.com>
2019-06-30 19:23:03 -0400

    Task19 updated

---
+++ Task19.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+struct V {
+   int num ;
+   bool tx ; 
+   char  prime[50];
+};
+struct E {
+   char  t[50];
+}; 
+struct primat {
+   char  sym[50];
+}; 
+ void main( ) 
+{
+   struct V objv;        
+   struct E obje;
+   struct primat objp;
+}
