fb2a21766c90d90dc47379d9205ba7b95be08386
Irfan Mohammed <mohammedirfan1460231@gmail.com>
2019-07-01 20:38:46 -0400

    Task34 updated

---
+++ Task34.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define temp  3
+struct V {
+   int number ;
+   bool f ; 
+   char  prim[50];
+};
+struct primitive {
+   char  sym[50];
+}; 
+struct X {
+   char  varnam[50];
+};
+struct E {
+   char  tag[10];
+   char  exp[10];
+   int num1;
+   int num2;
+}item[temp];
+; 
+
+ void main( ) 
+{        
+   struct V objd;
+   struct primitive objc;
+   struct X objf;        
+   
+}  
