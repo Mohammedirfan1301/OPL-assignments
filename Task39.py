8f47c18238e8dffe624450c8df9cc429b168102d
Irfan Mohammed <mohammedirfan1460231@gmail.com>
2019-07-01 22:55:30 -0400

    Task 39 updated

---
+++ Task39.py
@@ -0,0 +1,12 @@
+class E :
+    def __init__(self,list):
+        self.list = list
+class V:
+    def __init__(self,values):
+        self.values = values
+class prim:
+    def __init__(self,opernd):
+        self.opernd = opernd
+class X :
+    def __init__ (self,varname):
+        self.varname = varname
